ce0b4477de98e0433f64424032ab31cd568ebefa
Emanuel <emisoltuzu@gmail.com>
2024-11-16 19:30:51 +0000

    WOOO

--- modificat.sh
+++ modificat.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ipcorect='nslookup $1 $3 | awk {}'
+nume=$1
+ip_ver=$2
+server_dns=$3
+actual_ip=$(nslookup "$nume" "$server_dns" | grep -A 1 "Name:" | tail -n 1 | awk '{print $2}')
+if [ "$actual_ip" != "$ip_ver" ]; then
+	echo "Bogus IP for $nume!"
+fi
